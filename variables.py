dcdc2387ab1eaa434ddfffc510d3c93b5fbd42c6
LSF970 <Lfairbrass@spartaglobal.com>
2023-01-27 15:52:38 +0000

    First commit

---
+++ variables.py
@@ -0,0 +1,37 @@
+# Variables
+
+# variable_name = variable_data
+
+a = 1 #int
+b = 2 #int
+c = 3.5 #float
+hi = "Hello World!" #string
+
+
+# Comment out multiple lines at the same time 'Ctrl + /'
+
+# print(hi)
+# print(a)
+
+# print(1 + 2)
+# print(a + b)
+
+# the type method
+
+# print(type(hi))
+
+# Overwriting variables
+
+# print(a)
+# a = 4
+# print(a)
+
+# Getting user input
+print("Hi, what is your name?")
+name = input()
+print("hi")
+print(name)
+
+# Make a program that takes the users name, age and DOB and prints back out.
+# When you are done, push to GitHub and share in chat.
+
